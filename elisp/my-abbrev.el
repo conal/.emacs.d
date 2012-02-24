10bbd81397a681579eb05edf7a21c4f927923cfe
Conal Elliott <conal@conal.net>
2012-02-24 11:14:30 -0800

    improve in-word test for perhaps-expand-abbrev

--- elisp/my-abbrev.el
+++ elisp/my-abbrev.el
@@ -76,9 +76,10 @@ p")
 
 
 (defun perhaps-expand-abbrev ()
-  "Do expand-abbrev if abbrev-mode is true, but not if we're looking at a
-letter.  This test should really use the same criterion as the normal automatic
-abbreviation expansion mechanism."
+  "Do expand-abbrev if abbrev-mode is true, but not if we're inside a word."
   (interactive)
-  (when (and abbrev-mode (not (looking-at "[a-zA-Z]")))
-      (expand-abbrev)))
+  (when (and abbrev-mode
+             (looking-at "\\>")
+             )
+    (expand-abbrev))
+  )
